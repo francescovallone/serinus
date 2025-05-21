435da20c7b6a6da0493bbef274733531821bbd4e
Francesco Vallone <vallonefrancesco587@gmail.com>
2025-05-21 11:04:55 +0200

    fix(#177): fix issue with compilation when dist folder is missing, add method to fetch the dockerfile from repository

--- packages/serinus_cli/lib/src/commands/deploy_command.dart
+++ packages/serinus_cli/lib/src/commands/deploy_command.dart
@@ -1,3 +1,4 @@
+import 'dart:convert';
 import 'dart:io';
 
 import 'package:args/command_runner.dart';
@@ -63,10 +64,36 @@ class DeployCommand extends Command<int> {
       final content = Map<String, dynamic>.from(loadYaml(configContent) as Map);
       entrypoint = content['entrypoint'] as String;
     }
+    final dockerFileContent = dockerFileFromRepository(entrypoint, output, port);
     File(path.join(Directory.current.path, 'Dockerfile'))
         .writeAsStringSync(dockerFile(entrypoint, output, port));
     progress.complete('Dockerfile created');
     return ExitCode.success.code;
+  }
+
+  Future<String?> dockerFileFromRepository(
+    String entrypoint,
+    String output,
+    String port,
+  ) async {
+    final client = HttpClient();
+    final request = await client.getUrl(
+      Uri.parse('https://raw.githubusercontent.com/francescovallone/serinus/refs/heads/main/utils/Dockerfile.base'),
+    );
+    final response = await request.close();
+    if(response.statusCode != 200) {
+      _logger.err(
+        'Failed to fetch Dockerfile from repository fallback to cli version',
+      );
+      return null;
+    }
+    final responseBody = await response.transform(utf8.decoder).join();
+    final dockerFileContent = responseBody
+        .replaceAll('{{$entrypoint}}', entrypoint)
+        .replaceAll('{{$output}}', output)
+        .replaceAll('{{$port}}', port);
+
+    return dockerFileContent;    
   }
 
   String dockerFile(
@@ -85,6 +112,7 @@ RUN dart pub get
 COPY . .
 
 RUN dart pub get --offline
+RUN mkdir -p dist
 RUN dart compile exe $entrypoint -o dist/$output
 
 FROM scratch
