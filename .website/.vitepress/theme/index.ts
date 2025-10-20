e3410838af4067070bc5f3dc85bc412ced1040e1
Francesco Vallone <vallonefrancesco587@gmail.com>
2025-10-20 22:47:35 +0200

    docs(2.0): improve documentation reponsiveness

--- .website/.vitepress/theme/index.ts
+++ .website/.vitepress/theme/index.ts
@@ -1,8 +1,6 @@
 import DefaultTheme from 'vitepress/theme'
 import './style.css'
-import { h } from 'vue'
-import ShareRow from '../../components/share_row.vue'
-import Layout from './Layout.vue'
+import Layout from './layout.vue'
 
 export default {
     extends: DefaultTheme,
