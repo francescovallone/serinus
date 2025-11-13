d4822d84f714695bc36002a70ea631cba01cee29
Francesco Vallone <vallonefrancesco587@gmail.com>
2025-11-13 21:53:00 +0100

    chore: add analyze to the example

--- packages/serinus_openapi/example/lib/app_module.dart
+++ packages/serinus_openapi/example/lib/app_module.dart
@@ -14,6 +14,7 @@ class AppModule extends Module {
                 version: '1.0.0',
                 description: 'An example of Serinus with OpenAPI integration',
               ),
+              analyze: true
             )
           ],
           controllers: [AppController()],
