a2ec110f477bde45c0f30afebbb8358fc68bc8cf
Francesco Vallone <vallonefrancesco587@gmail.com>
2025-12-12 11:20:15 +0100

    chore: add auth_jwt example

---
+++ examples/auth_jwt/bin/auth_jwt.dart
@@ -0,0 +1,5 @@
+import 'package:auth_jwt/auth_jwt.dart' as auth_jwt;
+
+void main(List<String> arguments) {
+  auth_jwt.bootstrap();
+}
---
+++ examples/auth_jwt/lib/auth_jwt.dart
@@ -0,0 +1,55 @@
+import 'dart:convert';
+
+import 'package:frontier_jwt/frontier_jwt.dart';
+import 'package:frontier_jwt/jwt_strategy.dart';
+import 'package:serinus/serinus.dart';
+import 'package:serinus_config/serinus_config.dart';
+import 'package:serinus_frontier/serinus_frontier.dart';
+
+class AppController extends Controller {
+  AppController() : super('/') {
+    on<String, dynamic>(Route.get('/'), (RequestContext context) async {
+      return 'Hello World - ${jsonEncode(context['frontier_response'])}';
+    });
+  }
+
+  @override
+  List<Metadata> get metadata => [
+    GuardMeta('jwt'),
+  ];
+}
+
+class AppModule extends Module {
+  AppModule() : super(
+    imports: [
+      ConfigModule(),
+      Module.composed(
+        (CompositionContext context) async {
+          final configService = context.use<ConfigService>();
+          return FrontierModule([
+            JwtStrategy(
+              JwtStrategyOptions(
+                SecretKey(configService.getOrNull('JWT_SECRET') ?? 'default_secret'), 
+                jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken()
+              ),
+              (options, jwt, done) async {
+                final decodedJwt = jwt as JWT;
+                return done(decodedJwt.payload);
+              }
+            )
+          ]);
+        },
+        inject: [
+          ConfigService
+        ]
+      )
+    ],
+    controllers: [AppController()]
+  );
+}
+
+
+Future<void> bootstrap() async {
+  final app = await serinus.createApplication(entrypoint: AppModule());
+  await app.serve();
+}
