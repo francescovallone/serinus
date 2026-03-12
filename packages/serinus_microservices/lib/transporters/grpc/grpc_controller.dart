647bb0348d234f8c918b67395e38c08156dd4483
Francesco Vallone <vallonefrancesco587@gmail.com>
2026-03-12 15:46:17 +0100

    feat(microservices): change the whole approach to grpc microservices

--- packages/serinus_microservices/lib/transporters/grpc/grpc_controller.dart
+++ packages/serinus_microservices/lib/transporters/grpc/grpc_controller.dart
@@ -2,6 +2,20 @@ import 'package:grpc/grpc.dart';
 import 'package:protobuf/protobuf.dart';
 import 'package:serinus/serinus.dart';
 
+/// The [GrpcServiceController] allows defining gRPC services as Serinus Controllers.
+/// 
+/// It extends the base [Controller] class and includes a reference to the gRPC [Service] it represents. This controller can be used to group related gRPC methods together, making it easier to manage and organize your gRPC services within the Serinus framework.
+class GrpcServiceController extends Controller {
+
+  /// The [service] property contains the gRPC service definition that this controller represents.
+  final Service service;
+
+  /// Creates a gRPC service controller.
+  GrpcServiceController({
+    required this.service,
+  }): super(service.$name);
+}
+
 /// The [GrpcRoute] class is the gRPC
 class GrpcRoute extends Route {
   /// The [serviceName] property contains the name of the gRPC service.
